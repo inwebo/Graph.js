17519919eb84a0362a49a199d33af9b0c052e88f
inwebo <github@inwebo.net>
2020-12-20 13:55:43 +0100

    Update with _key

--- docs/assets/js/demos/demo-1.js
+++ docs/assets/js/demos/demo-1.js
@@ -7,29 +7,28 @@ const demo1 = () => {
 
     // Graph
     const nodes = new Map([
-        ['A', new Node()],
-        ['B', new Node()],
-        ['C', new Node()],
-        ['D', new Node()],
-        ['E', new Node()],
+        ['A', new Node('A')],
+        ['B', new Node('B')],
+        ['C', new Node('C')],
+        ['D', new Node('D')],
+        ['E', new Node('E')],
     ]);
 
     const graph = new Graph(nodes);
 
     // BiDirectionnal
-    nodes.get('A').addNeighbor('B', nodes.get('B'));
-    nodes.get('B').addNeighbor('A', nodes.get('A'));
+    nodes.get('A').addNeighbor(nodes.get('B'));
+    nodes.get('B').addNeighbor(nodes.get('A'));
 
-    nodes.get('E').addNeighbor('D', nodes.get('D'));
-    nodes.get('D').addNeighbor('E', nodes.get('E'));
+    nodes.get('E').addNeighbor(nodes.get('D'));
+    nodes.get('D').addNeighbor(nodes.get('E'));
 
-    // !BiDirectionnal
-    nodes.get('A').addNeighbor('D', nodes.get('D'));
-    nodes.get('C').addNeighbor('E', nodes.get('E'));
+    // Directed
+    nodes.get('A').addNeighbor(nodes.get('D'));
+    nodes.get('C').addNeighbor(nodes.get('E'));
 
-
-    nodes.get('E').addNeighbor('B', nodes.get('B'));
-    nodes.get('E').addNeighbor('A', nodes.get('A'));
+    nodes.get('E').addNeighbor(nodes.get('B'));
+    nodes.get('E').addNeighbor(nodes.get('A'));
 
     const renderGraph = new RenderGraph(canvas);
 
